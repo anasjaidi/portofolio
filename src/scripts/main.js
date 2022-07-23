ccedfbd7aca80822a4a0dcdb81b40e2177dd579c
Anas jaidi <anas.jaidi@icloud.com>
2022-07-23 15:56:56 +0100

    push

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -14,37 +14,36 @@ menu.addEventListener('click', function myFunction() {
     }
   });
 
-const removeAllActiveLinks = (sections) => {
-  sections.forEach(section => {
-    const link = document.querySelector('.link-' + section)
+// const removeAllActiveLinks = (sections) => {
+//   sections.forEach(section => {
+//     const link = document.querySelector('.link-' + section)
 
-      if (!link) return ;
+//       if (!link) return ;
 
-      link.classList.remove('active');
-  })
-}
+//       link.classList.remove('active');
+//   })
+// }
 
-document.addEventListener('scroll', () => {
-  const sections = ["home", "about", "skills", "projects", "contact"]
-  const colors = ['#00ADB5', '#393E46', '#222831', '#393E46', '#222831']
+// document.addEventListener('scroll', () => {
+//   const sections = ["home", "about", "skills", "projects", "contact"]
+//   const colors = ['#00ADB5', '#393E46', '#222831', '#393E46', '#222831']
 
-  sections.forEach(section => {
-    if (inviewport(section)) {
-      const link = document.querySelector('.link-' + section)
-      const sidepanel = document.getElementById('aside')
+//   sections.forEach(section => {
+//     if (inviewport(section)) {
+//       const link = document.querySelector('.link-' + section)
+//       const sidepanel = document.getElementById('aside')
 
-      if (!link || !sidepanel) return ;
+//       if (!link || !sidepanel) return ;
 
-      removeAllActiveLinks(sections)
+//       removeAllActiveLinks(sections)
 
-      link.classList.add('active')
-      sidepanel.style.backgroundColor = colors[sections.indexOf(section)]
-    }
+//       link.classList.add('active')
+//       sidepanel.style.backgroundColor = colors[sections.indexOf(section)]
+//     }
 
-    if (section === 'skills') console.log('skills in viewport: ' + inviewport(section))
-  })
-})
-const ac = document.getElementsByClassName("ac")[0];
+//     if (section === 'skills') console.log('skills in viewport: ' + inviewport(section))
+//   })
+// })
 const home = document.getElementById('home');
 const about = document.getElementById('about');
 const skills = document.getElementById('skills');
@@ -56,10 +55,12 @@ const lkskills = document.getElementById('link-skills');
 const lkprojects = document.getElementById('link-projects');
 const lkcontact = document.getElementById('link-contact');
 lkhome.addEventListener('click', () => {
+  ac = document.getElementsByClassName("ac")[0];
   ac.classList.add("nac")
   ac.classList.remove("ac");
   home.classList.add("ac");
   home.classList.remove("nac");
+  aside.style.backgroundColor= "#00ADB5";
 });
 lkabout.addEventListener('click', () => {
   ac = document.getElementsByClassName("ac")[0];
@@ -67,6 +68,7 @@ lkabout.addEventListener('click', () => {
   ac.classList.remove("ac");
   about.classList.add("ac");
   about.classList.remove("nac");
+  aside.style.backgroundColor= "#393E46";
 });
 lkskills.addEventListener('click', () => {
   ac = document.getElementsByClassName("ac")[0];
@@ -74,16 +76,21 @@ lkskills.addEventListener('click', () => {
   ac.classList.remove("ac");
   skills.classList.add("ac");
   skills.classList.remove("nac");
+  aside.style.backgroundColor= "#222831";
 });
 lkprojects.addEventListener('click', () => {
+  ac = document.getElementsByClassName("ac")[0];
   ac.classList.add("nac")
   ac.classList.remove("ac");
   projects.classList.add("ac");
   projects.classList.remove("nac");
+  aside.style.backgroundColor= "#393E46";
 });
 lkcontact.addEventListener('click', () => {
+  ac = document.getElementsByClassName("ac")[0];
   ac.classList.add("nac")
   ac.classList.remove("ac");
   contact.classList.add("ac");
   contact.classList.remove("nac");
+  aside.style.backgroundColor= "#222831";
 });
